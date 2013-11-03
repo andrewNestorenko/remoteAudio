c2cbf76ca021bab6ab50e646481a50be2afc0b71
Andrew Nestorenko <andrew.nestorenko@gmail.com>
2013-11-03 18:57:03 +0200

    small style improvements in extension

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
     var muteSwitch = $('#mute-switch');
-    chrome.storage.local.get('localMute', function(result) {
+    chrome.storage.local.get('localMute', function (result) {
         muteSwitch.bootstrapSwitch('setState', result.localMute);
     });
     muteSwitch.on('switch-change', function (e, data) {
@@ -9,10 +9,8 @@ $(function() {
         chrome.storage.local.set({'localMute': !!value})
     });
 
-
     var enabledSwitch = $('#enabled-switch');
-
-    chrome.storage.local.get('enabled', function(result) {
+    chrome.storage.local.get('enabled', function (result) {
         enabledSwitch.bootstrapSwitch('setState', result.enabled);
     });
     enabledSwitch.on('switch-change', function (e, data) {
